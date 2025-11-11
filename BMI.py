815ec37173eea96bfac8b1f82e766cafed13887e
AstonLim <astonlim3101@gmail.com>
2025-11-11 16:16:20 +0800

    edited bmi calc

--- BMI.py
+++ BMI.py
@@ -1,7 +1,7 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
-    bmi = weight/(height*height)
+    bmi = weight/(height** 2)
     print("Your BMI is :" + str(bmi))
     if(bmi < 18.5):
         print("Weight class: Under Weight")
