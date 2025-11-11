0accc4d3fcb5d5e6c5e1be4ba52225a36570cff5
AstonLim <astonlim3101@gmail.com>
2025-11-11 16:12:45 +0800

    bmi calculator

---
+++ BMI.py
@@ -0,0 +1,13 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    print("Your BMI is :" + str(bmi))
+    if(bmi < 18.5):
+        print("Weight class: Under Weight")
+    if(18.5 <= bmi <= 25.0):
+        print("Weight class: Normal Weight")
+    else:
+        print("Weight class: Over Weight")
+
+calculate_bmi(weight=57, height=1.73)
