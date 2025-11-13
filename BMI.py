1f59d783938481e97b8366721c0a6edbebba5185
AstonLim <astonlim3101@gmail.com>
2025-11-13 10:40:27 +0800

    actually correct now

--- BMI.py
+++ BMI.py
@@ -1,18 +1,17 @@
 def calculate_bmi(height, weight):
-    print("Height: ", height) #can also use str(height) instead of ,height 
+    print("Height: ", height) #can also use +str(height) instead of ,height 
     print("weight: ", weight)
-    weightno = 0
     bmi = weight/height**2
     if bmi < 18.5:
         weightno = -1
         weightclass = "under weight"
-    if bmi > 25.0:
+    elif bmi > 25.0:
         weightno = 1
         weightclass = "over weight"
     else:
         weightno = 0
         weightclass = "normal weight"
-    print("bmi : ",bmi, "weight class:", weightclass, "value: ",weightno)
+    print("bmi : ",bmi, "weight class:", weightclass, "value: ",weightno) 
     return weightno
 
 print(calculate_bmi(1.67, 70))
