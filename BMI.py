647bc3fdfea30fa29dc0990caafacfd171a41d09
AstonLim <astonlim3101@gmail.com>
2025-11-13 09:47:51 +0800

    actual bmi calc the other was lab2.py stuff

--- BMI.py
+++ BMI.py
@@ -1,68 +1,17 @@
-def display_main_menu():
-    print("Sub: display_main_menu")
-    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
-
-
-def get_user_input():
-    arraylist = []
-    print("Sub: get_user_input")
-    numstring = input()
-    numlist = numstring.split(",")
-    for eachnum in numlist:
-        arraylist.append(float(eachnum))
-    return arraylist
-
-
-def calc_average(arraylist):
-    print("Sub: calc_average")
-    total = sum(arraylist)
-    average = total / len(arraylist)
-    print("Average = ", f"{average: .2f}")
-    return round(average, 2)
-
-
-def find_min_max(arraylist):
-    print("Sub: find_min_max")
-    templist = sort_temperature(arraylist)
-    min = templist[0]
-    max = templist[-1]
-    print("MIN = " + str(min) + " and MAX = " + str(max))
-    return [min, max]
-
-
-def sort_temperature(arraylist):
-    print("Sub: sort_temperature")
-    templist = arraylist.copy()
-    templist.sort()
-    print("Sorted Numbers = ", templist)
-    return templist
-
-
-def calc_median_temperature(arraylist):
-    print("Sub: calc_median_temperature")
-    templist = sort_temperature(arraylist)
-    numcount = len(templist)
-    if numcount % 2 == 1:  # Odd number of temp readings, median is the reading in the middle.
-        median = templist[numcount//2]
+def calculate_bmi(height, weight):
+    print("Height: ", height)
+    print("weight: ", weight)
+    weightno = 0
+    bmi = weight/height**2
+    if bmi < 18.5:
+        weightno = -1
+        weightclass = "under weight"
+    if bmi > 25.0:
+        weightno = 1
+        weightclass = "over weight"
     else:
-        median = (templist[numcount//2-1] + templist[numcount//2])/2
-    print("MEDIAN = ", median)
-    return median
+        weightno = 0
+        weightclass = "normal weight"
+    print("bmi : ",bmi, "weight class: ", weightclass, "value: ",weightno)
 
-
-
-
-
-def main():
-    display_main_menu()
-    numArray = get_user_input()
-    calc_average(numArray)
-    find_min_max(numArray)
-    calc_median_temperature(numArray)
-
-
-
-
-if __name__ == "__main__":
-    main()
-
+calculate_bmi(1.67, 70)
